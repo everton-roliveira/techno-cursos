fe63eba641b61fbb7d915b32b4cb23361be72c74
Everton Roberto <eoliveira@alelo.com.br>
2020-06-04 17:17:24 -0300

    feat: add imagem aprender, removido o logo desnecessário e iniciado a estrutura html da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    <h1>Home</h1>
     <page-loading v-if="loading"></page-loading>
     <transition>
-      <div v-if="data">{{data}}</div>
+      <div v-if="data" class="content">
+        <div>
+          <h1>Sobre a {{data.title}}</h1>
+          <p>{{data.description}}</p>
+          <router-link tag="button" class="btn" to="/cursos">Cursos</router-link>
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="rating in data.ratings" :key="rating.id">
+                <p>{{rating.name}}</p>
+                <p>{{rating.description}}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="@/assets/images/learn.png" alt="aprender" />
+      </div>
     </transition>
   </div>
 </template>
