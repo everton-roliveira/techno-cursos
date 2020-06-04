d18ac82ffbb0908b6c68634cf08ae584de5f9ff9
Everton Roberto <eoliveira@alelo.com.br>
2020-06-04 20:24:45 -0300

    refactor: separado em arquivos os estilos que seram reutilizaveis

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,6 @@ export default {
 </script>
 <style lang="scss">
 body {
-  @include font-family("primary");
-  margin: 0;
-  padding: 0;
   color: color(brand, secondary);
   a {
     @include font-weight(bold);
@@ -26,33 +23,6 @@ body {
     color: color(brand, secondary);
     &.router-link-active {
       color: color(brand, primary);
-    }
-  }
-  .container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-
-  .content {
-    display: grid;
-    grid-template-columns: 1fr minmax(200px, 400px);
-    grid-gap: 30px;
-
-    @media screen and(max-width: 600px) {
-      display: block;
-    }
-  }
-
-  .header-page {
-    margin-bottom: 40px;
-    &__title {
-      text-transform: uppercase;
-      margin-bottom: 10px;
-    }
-    &__description {
-      @include font-weight(bold);
-      font-size: 1rem;
     }
   }
 }
